9fa712a418ce054a394d3112cd4a42eefff49fd5
victorsc45 <victorlitzau@gmail.com>
2020-06-23 10:20:55 -0400

    uv index and conditions statement correct pulling from 2 url apis

--- script.js
+++ script.js
@@ -1,143 +1,186 @@
-// Initial array of movies
-var cities = [];
+$(document).ready(function () {
 
-// displayMovieInfo function re-renders the HTML to display the appropriate content
-function displayCityInfo() {
+    // Initial array of movies
+    var cities = [];
     let apiKey = "7401399c2c0acdc905b25bf3b17e2d14";
-    var city = $(this).attr("data-name");
-    var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
+    // displayMovieInfo function re-renders the HTML to display the appropriate content
+    function displayCityInfo() {
 
-    // Creating an AJAX call for the specific movie button being clicked
-    $.ajax({
-        url: queryURL,
+        var city = $(this).attr("data-name");
+        var queryURL = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + apiKey + "&units=imperial";
 
-        method: "GET"
-
-    }).then(function (response) {
-        // Log the queryURL
-        console.log(queryURL);
-
-        // Log the resulting object
-        console.log(response);
-        // Creating a div to hold the movie
-        //  var cityDiv = $("<div class='city'>");
-        console.log(response.city.name);
-        // Storing the rating data
-        //  var date = response.list.dt;
-
-
-        let Date = response.list[0].dt_txt;
-
-
-
-        console.log(moment(Date).format('MMMM Do, YYYY'));
-
-        // Creating an element to have the rating displayed
-        //  var hTwo = $("<h2>").text("(" + "Date: " + date + ")");
-        console.log(response.list[0].main.temp);
-        // Displaying the rating
-        // cityDiv.append(hTwo);+ '&deg;F');
-        console.log("humidity", response.list[0].main.humidity);
-        console.log("wind", response.list[0].wind.speed);
-        let lat = (response.city.coord.lat).toFixed(2);
-
-        let lon = (response.city.coord.lon).toFixed(2);
-
-        console.log("lat", lat);
-        console.log("lon", lon);
-        queryURL1 = "http://api.openweathermap.org/v3/uvi/" + lat + "," + lon + "/" + Date + ".json?appid=" + apiKey;
-
-
+        // Creating an AJAX call for the specific movie button being clicked
         $.ajax({
-            url: queryURL1,
+            url: queryURL,
 
             method: "GET"
 
         }).then(function (response) {
 
-            console.log("uv i hope", response.data, "or this is right", response);
+            let lat = (response.city.coord.lat);
 
+            let lon = (response.city.coord.lon);
 
-            //http://api.openweathermap.org/v3/uvi/40.7,-74.2/current.json?appid={your-api-key}
-            // Storing the release year    // Transfer content to HTML
-            ////$(".city").html("<h1>" + response.name + " Weather Details</h1>");
-            //$(".wind").text("Wind Speed: " + response.wind.speed);
-            //$(".humidity").text("Humidity: " + response.main.humidity);
-            //var pTemp = response.temp + "&units=imperial";
+            let queryURL2 = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + lat + "&lon=" + lon;
 
-            // Creating an element to hold the release year
-            //  var pTag1 = $("<p>").text("Temperature: " + pTemp);
+            $.ajax({
+                url: queryURL2,
 
-            // Displaying the release year
-            //   CityDiv.append(pTag1);
+                method: "GET"
 
-            // Storing the plot
-            //  var plot = response.Plot;
+            }).then(function (uvIresponse) {
 
-            // Creating an element to hold the plot
-            //  var pThree = $("<p>").text("Plot: " + plot);
+                console.log("this is UV index", uvIresponse.value);
 
-            // Appending the plot
-            //  movieDiv.append(pThree);
+                // Log the queryURL
+                console.log(queryURL);
 
-            // Retrieving the URL for the image
-            //   var imgURL = response.Poster;
+                // Log the resulting object
+                console.log(response);
+                // Creating a div to hold the movie
+                //  var cityDiv = $("<div class='city'>");
+                console.log(response.city.name);
+                // Storing the rating data
+                //  var date = response.list.dt;
 
-            // Creating an element to hold the image
-            //   var image = $("<img>").attr("src", imgURL);
+                console.log(response.list[0].dt_txt);
+                let Date = response.list[0].dt_txt;
+                console.log(moment(Date).format('MMMM Do, YYYY'));
 
-            // Appending the image
-            //   movieDiv.append(image);
+                // Creating an element to have the rating displayed
+                //  var hTwo = $("<h2>").text("(" + "Date: " + date + ")");
+                console.log(response.list[0].main.temp);
+                // Displaying the rating
+                // cityDiv.append(hTwo);+ '&deg;F');
+                console.log("humidity", response.list[0].main.humidity);
+                console.log("wind", response.list[0].wind.speed);
 
-            // Putting the entire movie above the previous movies
-            //   $("#movies-view").prepend(movieDiv);
-            // getUV();
+                console.log("lat", lat);
+                console.log("lon", lon);
+                let color = "green";
+                let UVindex = uvIresponse.value;
+                if (UVindex > 7) {
+                    color = "red";
+                }
+                else if (UVindex > 5) {
+                    color = "orange";
+                } else if (UVindex > 2) {
+                    color = "yellow"
+                } else {
+                    color = "green"
+                }
+
+            })
         })
-    });
-}
-// function getUV() {
-
-
-// }
-// Function for displaying movie data
-function renderButtons() {
-
-    // Deleting the movies prior to adding new movies
-    // (this is necessary otherwise you will have repeat buttons)
-    $("#buttons-view").empty();
-
-    // Looping through the array of movies
-    for (var i = 0; i < cities.length; i++) {
-
-        // Then dynamicaly generating buttons for each movie in the array
-        // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
-        var a = $("<button>");
-        // Adding a class of movie-btn to our button
-        a.addClass("city-btn");
-        // Adding a data-attribute
-        a.attr("data-name", cities[i]);
-        // Providing the initial button text
-        a.text(cities[i]);
-        // Adding the button to the buttons-view div
-        $("#buttons-view").append(a);
     }
-}
 
-// This function handles events where a movie button is clicked
-$("#add-city").on("click", function (event) {
-    event.preventDefault();
-    // This line grabs the input from the textbox
-    var city = $("#city-input").val().trim();
+    // var ajax1 = $.ajax({ 
+    //     dataType: "json",
+    //     url: queryURL,
+    //     async: true,
+    //     success: function(response) {}                     
+    //   });
 
-    // Adding movie from the textbox to our array
-    cities.push(city);
 
-    // Calling renderButtons which handles the processing of our movie array
+    //   var ajax2 = $.ajax({ 
+    //     dataType: "json",
+    //     url: queryURL2,
+    //     async: true,
+    //     success: function(response) {}  
+    //   });
+
+    //   $.when( ajax1 , ajax2  ).done(function( a1, a2 ) {
+    //      // a1 and a2 are arguments resolved for the page1 and page2 ajax requests, respectively.
+    //      // Each argument is an array with the following structure: [ data, statusText, jqXHR ]
+    //      var data = a1[ 0 ] + a2[ 0 ]; // a1[ 0 ] = "Whip", a2[ 0 ] = " It"
+    //      if ( /Whip It/.test( data ) ) {
+    //         alert( "We got what we came for!" );
+    //      }
+    //   });
+
+
+
+    //http://api.openweathermap.org/v3/uvi/40.7,-74.2/current.json?appid={your-api-key}
+    // Storing the release year    // Transfer content to HTML
+    ////$(".city").html("<h1>" + response.name + " Weather Details</h1>");
+    //$(".wind").text("Wind Speed: " + response.wind.speed);
+    //$(".humidity").text("Humidity: " + response.main.humidity);
+    //var pTemp = response.temp + "&units=imperial";
+
+    // Creating an element to hold the release year
+    //  var pTag1 = $("<p>").text("Temperature: " + pTemp);
+
+    // Displaying the release year
+    //   CityDiv.append(pTag1);
+
+    // Storing the plot
+    //  var plot = response.Plot;
+
+    // Creating an element to hold the plot
+    //  var pThree = $("<p>").text("Plot: " + plot);
+
+    // Appending the plot
+    //  movieDiv.append(pThree);
+
+    // Retrieving the URL for the image
+    //   var imgURL = response.Poster;
+
+    // Creating an element to hold the image
+    //   var image = $("<img>").attr("src", imgURL);
+
+    // Appending the image
+    //   movieDiv.append(image);
+
+    // Putting the entire movie above the previous movies
+    //   $("#movies-view").prepend(movieDiv);
+    // getUV();
+
+
+
+    // function getUV() {
+
+
+
+    // Function for displaying movie data
+    function renderButtons() {
+
+        // Deleting the movies prior to adding new movies
+        // (this is necessary otherwise you will have repeat buttons)
+        $("#buttons-view").empty();
+
+        // Looping through the array of movies
+        for (var i = 0; i < cities.length; i++) {
+
+            // Then dynamicaly generating buttons for each movie in the array
+            // This code $("<button>") is all jQuery needs to create the beginning and end tag. (<button></button>)
+            var a = $("<button>");
+            // Adding a class of movie-btn to our button
+            a.addClass("city-btn");
+            // Adding a data-attribute
+            a.attr("data-name", cities[i]);
+            // Providing the initial button text
+            a.text(cities[i]);
+            // Adding the button to the buttons-view div
+            $("#buttons-view").append(a);
+        }
+    }
+
+    // This function handles events where a movie button is clicked
+    $("#add-city").on("click", function (event) {
+        event.preventDefault();
+        // This line grabs the input from the textbox
+        var city = $("#city-input").val().trim();
+
+        // Adding movie from the textbox to our array
+        cities.push(city);
+
+        // Calling renderButtons which handles the processing of our movie array
+        renderButtons();
+    });
+
+    // Adding a click event listener to all elements with a class of "movie-btn"
+    $(document).on("click", ".city-btn", displayCityInfo);
+
+    // Calling the renderButtons function to display the initial buttons
     renderButtons();
 });
-
-// Adding a click event listener to all elements with a class of "movie-btn"
-$(document).on("click", ".city-btn", displayCityInfo);
-
-// Calling the renderButtons function to display the initial buttons
-renderButtons();
